fff4bcca367aa437522bfbb0cea2bd83d83dabf3
syfsyf <syfsyf@gmail.com>
2018-09-27 20:02:28 +0200

    Material UI

--- webapp/src/js/components/Phototool.jsx
+++ webapp/src/js/components/Phototool.jsx
@@ -2,9 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 import ProfileForm from "./ProfileForm"
 import ErrorComponent from "./ErrorComponent"
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 
 class Phototool extends React.Component {
+
+
+
   componentDidMount() {
     this.pullStatus()
     this.props.fetchAppInfo()
@@ -21,7 +34,16 @@ class Phototool extends React.Component {
   render() {
     console.log("PROPS", this.props)
     return (
-      <div>
+      <div className={styles.root}>
+
+            <AppBar position="static" color="default">
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  Phototool
+                </Typography>
+              </Toolbar>
+            </AppBar>
+
         <ErrorComponent error={this.props.main.error} />
         <ProfileForm profile={this.props.forms} startJob={this.props.startJob} />
         <div>status:{this.props.main.processStatus.progressLabel}</div>
