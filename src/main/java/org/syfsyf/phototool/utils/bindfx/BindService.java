149dd15830ed55798950e156a2ff2fe8f5610e87
syfsyf <syfsyf@gmail.com>
2020-01-20 20:23:17 +0100

    Java FX - gui, refactor.

--- src/main/java/org/syfsyf/phototool/utils/bindfx/BindService.java
+++
@@ -1,9 +0,0 @@
-package org.syfsyf.phototool.utils.bindfx;
-
-import java.util.List;
-
-public interface BindService {
-
-    void bind(List<String> properties,Object fxContainer,Object dto) throws BinderException;
-
-}
--- src/main/java/org/syfsyf/phototool/utils/bindfx/Binder.java
+++
@@ -1,7 +0,0 @@
-package org.syfsyf.phototool.utils.bindfx;
-
-public interface Binder {
-
-    boolean handle(Class fxControl,Class dtoClass);
-    void bind(String property,Object fxContainer,Object dtoObject) throws BinderException;
-}
